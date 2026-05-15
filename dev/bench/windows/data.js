01edee97db3f713602746fc1d6467a6741af13a1
github-action-benchmark <github@users.noreply.github.com>
2026-05-15 07:32:22 +0000

    add Windows Benchmarks (customSmallerIsBetter) benchmark result for cb048d7cd8a6fb283d5dd0584c0b3c2d0d104b2c

--- dev/bench/windows/data.js
+++ dev/bench/windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778827898216,
+  "lastUpdate": 1778830342653,
   "repoUrl": "https://github.com/hyperlight-dev/hyperlight-unikraft",
   "entries": {
     "Windows Benchmarks": [
@@ -649,6 +649,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "pandas (median)",
             "value": 707,
+            "unit": "ms"
+          },
+          {
+            "name": "density (per VM)",
+            "value": 6,
+            "unit": "MB"
+          },
+          {
+            "name": "snapshot (disk)",
+            "value": 393,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "danilochiarlone@gmail.com",
+            "name": "Dan Chiarlone",
+            "username": "danbugs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cb048d7cd8a6fb283d5dd0584c0b3c2d0d104b2c",
+          "message": "Merge pull request #38 from hyperlight-dev/release/0.2.0\n\nchore: bump version to 0.2.0",
+          "timestamp": "2026-05-15T00:23:35-07:00",
+          "tree_id": "940824eb31ed2288f9e9674e3a01bc68e82b4b00",
+          "url": "https://github.com/hyperlight-dev/hyperlight-unikraft/commit/cb048d7cd8a6fb283d5dd0584c0b3c2d0d104b2c"
+        },
+        "date": 1778830340811,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "hello_world (median)",
+            "value": 218,
+            "unit": "ms"
+          },
+          {
+            "name": "pandas (median)",
+            "value": 739,
             "unit": "ms"
           },
           {
